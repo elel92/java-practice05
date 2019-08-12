f8b77f3de5929819f8defc6841dc0e002b032578
BIT <BIT@DESKTOP-Q04GE57>
2019-08-12 19:11:14 +0900

    2번 완료

--- src/prob2/SmartPhone.java
+++ src/prob2/SmartPhone.java
@@ -17,7 +17,7 @@ public class SmartPhone extends MusicPhone {
 		System.out.println("앱실행");
 	}
 	
-	private void playMusic(){
+	protected void playMusic(){
 		System.out.println("다운로드해서 음악재생");
 	}
 }
